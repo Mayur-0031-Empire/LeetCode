92bb413be81179480b0269b8c21a49490cc64246
Mayur 0031 <mayurkoshti0031@gmail.com>
2026-01-01 09:40:47 +0530

    Time: 0 ms (100%), Space: 17.4 MB (86.09%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,15 +1,15 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        resulting_array = []
-        s = digits[-1] + 1
-        resulting_array.append(s % 10)
-        carry = s // 10
-        for i in range(len(digits) - 2, -1, -1):
-            s = digits[i] + carry
-            resulting_array.append(s % 10)
-            carry = s // 10
-        
+        result = []
+        n = len(digits)
+        carry = 1
+        for i in range(n - 1, -1, -1):
+            sum = digits[i] + carry 
+            digit = (sum) % 10
+            result.append(digit)
+            carry = sum // 10
         if carry == 1:
-            resulting_array.append(1)
-        resulting_array.reverse()
-        return resulting_array
+            result.append(carry)
+        result.reverse()
+        return result
+        
